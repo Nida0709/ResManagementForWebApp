8663419bb185f096794ad533723566ac5f180e0c
Nida0709 <rikutocbr0709@outlook.com>
2022-12-18 16:20:09 +0900

    ver1.0.3

--- webapp/main.py
+++ webapp/main.py
@@ -2,9 +2,17 @@ import os, sys, csv, phonenumbers, sqlite3
 from datetime import datetime
 import pandas as pd
 from webapp import app
-from flask import render_template, redirect, request, url_for
+from flask import render_template, redirect, request, url_for, flash
 
 DATABASE = 'database.db'
+
+
+
+
+
+
+
+
 
 
 @app.route('/')
@@ -12,6 +20,13 @@ def index():
     return render_template(
         'index.html',
     )
+
+
+
+
+
+
+
 
 
 
@@ -102,12 +117,18 @@ def table():
     reserves4 = [{'content': restext4}]
 
     return render_template(
-        'index.html',
+        'table.html',
         reservers1=reserves1,
         reservers2=reserves2,
         reservers3=reserves3,
         reservers4=reserves4
     )
+
+
+
+
+
+
 
 
 
@@ -122,51 +143,44 @@ def form():
 
 
 
+
+
+
+
+
 @app.route('/register', methods=['POST'])
 def register():
-    res = []
-    n_res = []
     name = request.form['name']
     tel = '(+81)' + str(request.form['tel'])
-    day = request.form['day']
+    date = request.form['day']
     n_hon = request.form['n_hon']
     n_kin = request.form['n_kin']
-    res.append(request.form['res1'])
-    n_res.append(request.form['n_res1'])
-    res.append(request.form['res2'])
-    n_res.append(request.form['n_res2'])
-    res.append(request.form['res3'])
-    n_res.append(request.form['n_res3'])
-    res.append(request.form['res4'])
-    n_res.append(request.form['n_res4'])
-    res.append(request.form['res5'])
-    n_res.append(request.form['n_res5'])
+    res1 = request.form['res1']
+    n_res1 = request.form['n_res1']
+    res2 = request.form['res2']
+    n_res2 = request.form['n_res2']
+    res3 = request.form['res3']
+    n_res3 = request.form['n_res3']
+    res4 = request.form['res4']
+    n_res4 = request.form['n_res4']
+    res5 = request.form['res5']
+    n_res5 = request.form['n_res5']
     other = request.form['other']
 
     tel_hyphen = phonenumbers.parse(tel, 'JP')
     str_tel = phonenumbers.format_number(tel_hyphen, phonenumbers.PhoneNumberFormat.NATIONAL)
 
-    dt = datetime.strptime(day, "%Y-%m-%dT%H:%M")
-    unixday = datetime.timestamp(dt)
+    dt_date = datetime.strptime(date, "%Y-%m-%dT%H:%M")
+    uni_date = datetime.timestamp(dt_date)
 
     if n_hon == '':
         n_hon = 0
     if n_kin == '':
         n_kin = 0
-
-    res_text = ''
-    if res[0] != '' and n_res[0] != '':
-        res_text = res[0] + 'x' + n_res[0]
-    for i in range(1, 5):
-        if res[i] != '' and n_res[i] != '':
-            res_text = res_text + 'nn' + res[i] + 'x' + n_res[i]
-    insertdata = [name, str_tel, unixday, n_hon, n_kin, res_text, other]
     
-    dataPath = os.getcwd() + os.sep + 'rm' + os.sep + 'rmDatabase.csv'
-    with open(dataPath, encoding='shift-jis') as f:           #scan to method_index
-        csvreader = csv.reader(f)
-        rmDatabase = [row for row in csvreader]
-    rmDatabase.append(insertdata)
-    df = pd.DataFrame(rmDatabase)
-    df.to_csv(dataPath, encoding='shift-jis', header=False, index=False)
+    con = sqlite3.connect(DATABASE)
+    con.execute("INSERT INTO reserves VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", \
+        [name, str_tel, uni_date, n_hon, n_kin, res1, n_res1, res2, n_res2, res3, n_res3, res4, n_res4, res5, n_res5, other])
+    con.commit()
+    con.close()
     return redirect(url_for('form'))
